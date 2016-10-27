6d4052e8e6f0d8637da367a86a9f1f0cf389015d
BFAlex <838318741@qq.com>
2016-10-27 15:05:32 +0800

    UICollectionView
    UICollectionView的简单创建和使用步骤

--- BFCollectionViewDemo/ViewController.m
+++ BFCollectionViewDemo/ViewController.m
@@ -8,7 +8,9 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#define kCollectionViewCellID @"CollectionViewCellID"
+
+@interface ViewController () <UICollectionViewDataSource, UICollectionViewDelegate>
 
 @end
 
@@ -16,12 +18,45 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self setupCollectionView];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - custom methods
+
+- (void)setupCollectionView {
+    // 创建一个layout布局
+    UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc] init];
+    // 设置布局方向为垂直布局
+//    layout.scrollDirection = UICollectionViewScrollDirectionVertical;
+    layout.scrollDirection = UICollectionViewScrollDirectionHorizontal;
+    // 设置每个item的大小为100*100
+    layout.itemSize = CGSizeMake(100, 100);
+    
+    // 创建UICollectionView,并制定一个布局策略layout
+    UICollectionView *collectionView = [[UICollectionView alloc] initWithFrame:self.view.bounds collectionViewLayout:layout];
+    // 设置代理
+    collectionView.delegate = self;
+    collectionView.dataSource = self;
+    
+    // 注册item类型（这里使用系统类型）
+    [collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:kCollectionViewCellID];
+    
+    [self.view addSubview:collectionView];
+}
+
+#pragma mark - delegate && data source methods
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    
+    return 32;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:kCollectionViewCellID forIndexPath:indexPath];
+    cell.backgroundColor = [UIColor colorWithRed:arc4random()%255/255.0 green:arc4random()%255/255.0 blue:arc4random()%255/255.0 alpha:1];
+    
+    return cell;
 }
 
 @end
